6b9c9ba2fd0003f8c35e9a43ef6e071d35835c2e
Bruce Ho <mitch6198@gmail.com>
2024-10-07 11:38:04 +0800

    add product.py

--- product.py
+++ product.py
@@ -3,7 +3,9 @@ while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
         break
-    price = input('請輸入商品價價格：')
+    price = int(input('請輸入商品價價格：'))
+    quantity = int(input('請輸入數量：'))
+    total = price*quantity
     #第一種寫法
     #p = []#小清單
     #p.append(name)#新增商品名稱至小清單
@@ -12,6 +14,8 @@ while True:
     #p = [name,price]
     #products.append(p)
     #最精簡的寫法
-    products.append([name,price])
-
+    products.append([name,price,quantity,total])
 print(products)
+
+for p in products:
+	print('商品：',p[0],'價格：',p[1],'數量：',p[2],'總金額',p[3])
